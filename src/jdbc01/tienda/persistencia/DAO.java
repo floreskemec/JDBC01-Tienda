72369c5e18fc0898707c501ec72deb5bf2be0967
Gonzalo Flores Kemec <floreskemec@gmail.com>
2022-02-10 21:23:02 -0300

    primer commit

---
+++ src/jdbc01/tienda/persistencia/DAO.java
@@ -0,0 +1,78 @@
+package jdbc01.tienda.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DAO {
+
+    protected Connection conexion = null;
+    protected Statement sentencia = null;
+    protected ResultSet resultado = null;
+
+    private final String USUARIO = "root";
+    private final String PASSWORD = "root";
+    private final String DATABASE = "tienda";
+    private final String DRIVER = "com.mysql.jdbc.driver";
+
+    protected void conectarBase() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName(DRIVER);
+            String urlBaseDeDatos = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false";
+            conexion = DriverManager.getConnection(urlBaseDeDatos, USUARIO, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw ex;
+
+        }
+    }
+
+    protected void desconectarBase() {
+        try {
+            if (conexion != null) {
+                 conexion.close();
+            }
+            if (sentencia != null) {
+                sentencia.close();
+            }
+            if (resultado != null) {
+                resultado.close();
+            }
+            
+        } catch (Exception e) {
+        }
+    }
+    
+     protected void insertarModificarEliminar(String sql) throws Exception {
+        try {
+            conectarBase();
+            sentencia = conexion.createStatement();
+            sentencia.executeUpdate(sql);
+        } catch (SQLException ex) {
+            // conexion.rollback();
+            /*  Descomentar la linea anterior si desean tener en cuenta el rollback.
+                Correr el siguiente script en Workbench
+            
+                SET autocommit=1;
+                COMMIT;
+            
+                **Sin rollback igual anda */
+             
+            throw ex;
+        } finally {
+            desconectarBase();
+        }
+    }
+
+    protected void consultarBase(String sql) throws Exception {
+        try {
+            conectarBase();
+            sentencia = conexion.createStatement();
+            resultado = sentencia.executeQuery(sql);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+}
